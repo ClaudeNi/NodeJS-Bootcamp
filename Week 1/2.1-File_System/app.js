ab39e108fd3e83e68dafac742a5cc9964c86fcce
Claude Ni <49127719+ClaudeNi@users.noreply.github.com>
2022-01-09 11:36:12 +0200

    Add files via upload

--- Week 1/2.1-File_System/app.js
+++ Week 1/2.1-File_System/app.js
@@ -4,5 +4,4 @@ fs.writeFileSync("text.txt", "file");
 fs.copyFileSync("text.txt", "copy.txt");
 fs.renameSync("text.txt", "original.txt");
 const list = fs.readdirSync("./");
-console.log(list);
-fs.appendFileSync("copy.txt", "\nthis is the copy");
+fs.appendFileSync("copy.txt", `\n${list}`);
